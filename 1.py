e40137db54d7960dba924d857bccadefba56b5e3
alex <jiahangsocket@163.com>
2015-01-09 08:01:35 +0800

    first commit

---
+++ 1.py
@@ -0,0 +1,21 @@
+import datetime
+import os
+
+#
+
+def set_sys_date(year, month, day):
+    print('date %04d/%02d/%02d' % (year, month, day))
+    os.system('date %04d/%02d/%02d' % (year, month, day))
+
+def set_sys_time(hour, minute, second):
+    print('time %02d:%02d:%02d' % (hour, minute, second))
+    os.system('time %02d:%02d:%02d' % (hour, minute, second))
+
+def commit(msg):
+    os.system('git add .')
+    os.system('git commit -m %s' % (msg))
+
+if __name__ == '__main__':
+    set_sys_date(2015,1,9) #设置那一天
+    set_sys_time(8,0,0)   #设置时分秒
+    commit("first commit") #提交
---
+++ cmake-build-debug/CMakeFiles/c_college.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_college.dir/main.cpp.obj"
+  "c_college.exe"
+  "c_college.exe.manifest"
+  "c_college.pdb"
+  "libc_college.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c_college.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
