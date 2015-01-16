e40f9d83ecf18b55e51ce59f020878a16a72f0f3
alex <jiahangsocket@163.com>
2015-01-16 19:10:12 +0800

    结构体

---
+++ dynamic_memory_cross_function/DynamicMemoryCrossFunction.cpp
@@ -0,0 +1,29 @@
+﻿// dynamic_memory_cross_function.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+#include <stdio.h>
+
+struct Student {
+	int age;
+	float score;
+	char sex;
+};
+
+/***
+  结构体
+	为什么需要结构体
+		为了表示一些复杂的事物，而普通的基本类型无法满足实际要求
+	什么叫做结构体
+		把一些基本类型数据组合在一起形成一个新的数据类型，这个叫做结构体
+**/
+
+int main(void)
+{
+	struct Student st = {80,66.6,'F'};
+//	int age;
+//	float score;
+//	char name[100];
+
+//	int age2;
+//	float score2;
+//	char name2[100];
+	return 0;
+}
