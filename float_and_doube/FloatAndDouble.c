5146d0ee3819602faa3f90864b0f271b1156abb3
alex <jiahangsocket@163.com>
2015-01-18 20:11:12 +0800

    浮点型存储

---
+++ float_and_doube/FloatAndDouble.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+//sub 1
+
+int main(void) {
+    //不同类型数据之间相互赋值的问题
+    int i = 45;
+    long j = 102345;
+    i = j;
+
+    printf("%ld %d\n", i, j);
+
+    float x = 6.6;
+    double y = 8.8;
+
+    printf("%f %lf\n", x, y);
+    return 0;
+}
+
