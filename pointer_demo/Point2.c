fe0c4fbe0ae9d60f2e964e1832f152e4e2248a89
alex <jiahangsocket@163.com>
2015-02-28 22:20:21 +0800

    指针演示

---
+++ pointer_demo/Point2.c
@@ -0,0 +1,19 @@
+// 指针2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    int * p;
+
+    int i = 5;
+
+    *p = i;
+
+    printf("%d\n",*p);
+
+    return 0;
+}
+
+
---
+++ pointer_demo/Point4.c
@@ -0,0 +1,48 @@
+// 指针4.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//互换两个数字
+
+#include <stdio.h>
+
+
+
+void huhuan1(int a,int b) {  //这个函数不能完成互换功能
+    int t;
+
+    t = a;
+    a = b;
+    b = t;
+
+    return;
+}
+
+
+void huhuan2(int * p,int * q) { //这个函数不能互换
+    int * t;
+
+    t = p;
+    p = q;
+    q = t;
+}
+
+void huhuan3(int * p,int * q) { //可以互换主函数a,b的值
+    int t;
+    t = *p;
+    *p = *q;
+    *q = t;
+}
+
+
+
+int main(void)
+{
+    int a = 3;
+    int b = 5;
+
+    //huhuan1(a,b);  // 不能完成互换
+
+    huhuan2(&a,&b); // 不能完成互换
+
+    printf("a = %d,b = %d \n",a,b);
+
+    return 0;
+}
