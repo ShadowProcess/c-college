32c48129ac05b4cd041a34a9181b7dcc549ca4fd
alex <jiahangsocket@163.com>
2015-03-18 19:26:21 +0800

    递归

---
+++ recursion/Recursion.cpp
@@ -0,0 +1,22 @@
+﻿// recursion.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <stdio.h>
+
+int f(int n) {
+	n += 2;
+	return n;
+}
+
+
+
+int main(void)
+{
+	int val;
+	val = f(5);
+	printf("val = %d\n",val);
+
+	return 0;
+}
+
+
