5e768238af868653ee7cc64e6c0732eb8bf37286
alex <jiahangsocket@163.com>
2015-04-04 13:26:21 +0800

    复习函数

---
+++ review_function/ReviewFunction.cpp
@@ -0,0 +1,24 @@
+﻿// review_function.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+/**
+
+	在一个函数内部的变量,只在函数内部使用
+
+**/
+
+#include <stdio.h>
+
+
+void f(void) { //括号里的void表示:不需要参数
+
+}
+
+int f(int i) {    //这个函数返回值为10，因为返回值以方法名前的类型为主
+	return 10.8;
+}
+
+
+int main(void)
+{
+	return 0;
+}
+
