6fe433df667a15ae46a1b501a179aec680a82551
alex <jiahangsocket@163.com>
2015-05-14 21:21:21 +0800

    cmake

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2015,5,13)    #设置那一天
+    set_sys_date(2015,5,14)    #设置那一天
     set_sys_time(21,21,21)     #设置时分秒
-    commit("模块2加入cmake")              #想要深色点，最低三个提交
+    commit("cmake")     #想要深色点，最低三个提交
