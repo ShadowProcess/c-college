fcee7e8b7d859794bb98f6d919adbd86726f0bb5
alex <jiahangsocket@163.com>
2015-06-12 15:20:21 +0800

    return和break区别

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2015,6,11)    #设置那一天
-    set_sys_time(11,20,21)    #设置时分秒
-    commit("调试代码")      #想要深色点，最低三个提交
+    set_sys_date(2015,6,12)    #设置那一天
+    set_sys_time(15,20,21)    #设置时分秒
+    commit("return和break区别")      #想要深色点，最低三个提交
--- return_break/return和break的区别.cpp
+++ return_break/return和break的区别.cpp
@@ -1,5 +1,4 @@
-﻿// return_break.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-/**
+﻿/**
 	break 
 		1.是用来终止循环和switch的
 	
@@ -7,7 +6,7 @@
 		1.用来终止函数的,向主调函数返回表达式的值 
 		2.如果表达式为空，则只终止函数，不向被调函数返回任何值
 **/
-
+#include <iostream>
 void f(void) {
 	int i;
 	for (i = 0; i < 5;++i) {
@@ -17,12 +16,9 @@ void f(void) {
 	printf("同之门好!\n");
 }
 
-#include <iostream>
-
 int main(void)
 {
 	f();
-
 	return 0;
 }
 
