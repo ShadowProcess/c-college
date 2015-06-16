51d0960ff63000f8cd46a98239c8baad594fb3fa
alex <jiahangsocket@163.com>
2015-06-16 18:20:21 +0800

    printlf

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2015,6,15)    #设置那一天
-    set_sys_time(17,20,21)    #设置时分秒
-    commit("冒泡")      #想要深色点，最低三个提交
+    set_sys_date(2015,6,16)    #设置那一天
+    set_sys_time(18,20,21)    #设置时分秒
+    commit("printlf")      #想要深色点，最低三个提交
--- printlf_demo/PrintlfDemo.h
+++ printlf_demo/PrintlfDemo.h
@@ -1,8 +1,3 @@
-//
-// Created by Alex on 2019/11/29.
-//
-
 #ifndef CDEMO_PRINTLFDEMO_H
 #define CDEMO_PRINTLFDEMO_H
-
 #endif //CDEMO_PRINTLFDEMO_H
