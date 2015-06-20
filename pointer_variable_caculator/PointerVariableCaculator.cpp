3373167c56e2c6b2113a1e894a17443035092b5c
alex <jiahangsocket@163.com>
2015-06-20 20:21:21 +0800

    元素的距离

--- pointer_variable_caculator/PointerVariableCaculator.cpp
+++ pointer_variable_caculator/PointerVariableCaculator.cpp
@@ -1,7 +1,4 @@
-﻿// pointer_variable_caculator.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿#include <iostream>
 
 int main(void)
 {
@@ -9,13 +6,10 @@ int main(void)
 	int * p;
 	int * q;
 
-
 	p = &a[1];
-
 	q = &a[4];
 
 	printf("两个元素相隔的单元: %d", q - p);
-
 	return 0;
 }
 
