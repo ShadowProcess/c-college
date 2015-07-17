a4f7a47ce34ec1bf9fe275e5b1522d993833be69
alex <jiahangsocket@163.com>
2015-07-17 19:21:21 +0800

    数组的地址

--- pointer_and_array/PointerAndArray.cpp
+++ pointer_and_array/PointerAndArray.cpp
@@ -1,18 +1,11 @@
-﻿// pointer_and_array.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-
-//pointer_and_array
-
-#include <stdio.h>
+﻿#include <stdio.h>
 
 int main(void)
 {
 	int a[5];    // a是数组名 5是数组元素个数 元素就是变量
-	
 
 	printf("%#X\n",&a[0]);
-
 	printf("%#X", a); //a是数组第一个元素的地址
-
 	return 0;
 }
 
