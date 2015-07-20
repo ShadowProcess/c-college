ab246ddb59179f2100344f137afc2e6d1d2b8c44
alex <jiahangsocket@163.com>
2015-07-20 17:14:11 +0800

    复习2

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2015,7,19)    #设置那一天
-    set_sys_time(19,14,11)    #设置时分秒
-    commit("复习1")        #想要深色点，最低三个提交
+    set_sys_date(2015,7,20)    #设置那一天
+    set_sys_time(17,14,11)    #设置时分秒
+    commit("复习2")        #想要深色点，最低三个提交
--- modify_the_value_of_a_variable_by_pointer/ModifyTheValueOf VariableByPointer.cpp
+++ modify_the_value_of_a_variable_by_pointer/ModifyTheValueOf VariableByPointer.cpp
@@ -1,6 +1,4 @@
-﻿// modify_the_value_of_a_variable_by_pointer.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-
-#include <iostream>
+﻿#include <iostream>
 
 void f(int * p) {
 	*p = 6;
@@ -11,7 +9,6 @@ int main(void)
 	int i = 5;
 	f(&i);
 	printf("%d",i);
-
 	return 0;
 }
 
