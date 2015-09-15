45b2d6660df2827bda970ce772820bc1d6b2f187
alex <jiahangsocket@163.com>
2015-09-15 19:10:11 +0800

    33

--- 1.py
+++ 1.py
@@ -16,4 +16,4 @@ def commit(msg):
 if __name__ == '__main__':
     set_sys_date(2015,9,15)    #设置那一天
     set_sys_time(19,10,11)     #设置时分秒
-    commit("11")             #想要深色点，最低三个提交
+    commit("33")             #想要深色点，最低三个提交
