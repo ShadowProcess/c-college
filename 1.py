68089b4a64d9dbcddb7c082968ed1d26fe7857d8
alex <jiahangsocket@163.com>
2015-10-12 20:51:27 +0800

    日常作业

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 import os
 
 def set_sys_date(year, month, day):
@@ -15,5 +16,5 @@ def commit(msg):
 
 if __name__ == '__main__':
     set_sys_date(2015,10,12)    #设置那一天
-    set_sys_time(20,10,11)     #设置时分秒
+    set_sys_time(random.randint(8,23),random.randint(0,60),random.randint(0,60))
     commit("日常作业")             #想要深色点，最低三个提交
