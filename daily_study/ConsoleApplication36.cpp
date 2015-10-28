988afe13bacbe2e51971d4e709c61a0b994f0767
alex <jiahangsocket@163.com>
2015-10-28 12:31:36 +0800

    定义结构体

--- daily_study/ConsoleApplication36.cpp
+++ daily_study/ConsoleApplication36.cpp
@@ -1,7 +1,4 @@
-﻿// ConsoleApplication36.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <stdio.h>
+﻿#include <stdio.h>
 
 struct Student {
 	int sid;
@@ -11,11 +8,9 @@ struct Student {
 
 //形参指针类型
 void f(struct Student * pSt) {
-
 }
 
 void g(struct Student st) {
-
 }
 
 int main(void)
