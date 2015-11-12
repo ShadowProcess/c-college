67d272006072c8dd6c3e0578d51061227ca81072
alex <jiahangsocket@163.com>
2015-11-12 14:24:24 +0800

    自动增长因子

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2015,11,10)   #设置那一天
+    set_sys_date(2015,11,12)   #设置那一天
     set_sys_time()
-    commit("让系统分配内存")          #想要深色点，最低三个提交
+    commit("自动增长因子")          #想要深色点，最低三个提交
