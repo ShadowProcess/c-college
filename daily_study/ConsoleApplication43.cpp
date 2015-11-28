789dd2e68e21aa4925e739f51d8736f4fc7bd670
alex <jiahangsocket@163.com>
2015-11-28 19:09:15 +0800

    数据域

--- daily_study/ConsoleApplication43.cpp
+++ daily_study/ConsoleApplication43.cpp
@@ -1,10 +1,6 @@
-﻿// ConsoleApplication43.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <stdio.h>
+﻿#include <stdio.h>
 
 typedef struct Node {
-
 	int data; //数据域
 	struct Node * pNext; //指针域
 } Node, *PNODE; //Node 等价于struct Node    PNODE等价于struct Node *类型
@@ -12,8 +8,6 @@ typedef struct Node {
 
 int main(void)
 {
-   
-
 	return 0;
 }
 
